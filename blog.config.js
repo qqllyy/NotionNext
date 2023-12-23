1c0c1f0a9db46af61543b32276d4b4d120c46da2
qqllyy <46415896+qqllyy@users.noreply.github.com>
2023-12-24 02:34:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://necessary-pumpkin-e84.notion.site/2a04bd28f2d7414a808f43a2c3fd0724?v=8439298165694ec2bcbb72f6e55817d1&pvs=4
   NOTION_PAGE_ID:
       process.env.NOTION_PAGE_ID || '8439298165694ec2bcbb72f6e55817d1&pvs=4',
-  PSEUDO_STATIC: false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
+  PSEUDO_STATIC: true, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 5单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
   THEME: process.env.NEXT_PUBLIC_THEME || 'next', // 主题， 支持 ['next','hexo',"fukasawa','medium','example'] @see https://preview.tangly1024.com
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
