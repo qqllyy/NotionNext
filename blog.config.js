28c1b4fdb90e2b1360315737c968c336d3f8ce51
qqllyy <46415896+qqllyy@users.noreply.github.com>
2023-12-24 02:20:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '钰郎哥ᯤ¹🐬知行合一', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '“点灯十年，照亮未来！十年磨剑无人知，一朝出鞘惊天下。”@钰郎哥 ', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://necessary-pumpkin-e84.notion.site/3c1b505f52624ae58c4e50bd23770bd3?v=951c99d657ab40caa2ab1bb066986982', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://boke.qqllyy.cloudns.org/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '钰郎哥ᯤ¹🐬, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
