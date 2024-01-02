05d31d1aad5bd51a715d8ca3fbf2a0b3ad1529a0
qqllyy <46415896+qqllyy@users.noreply.github.com>
2024-01-02 23:41:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,10 +16,10 @@ const BLOG = {
   GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是钰郎哥！“点灯十年，照亮未来！十年磨剑无人知，一朝出鞘惊天下。”@钰郎哥 ,欢迎来到我的博客🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '钰郎哥ᯤ¹🐬知行合一', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '钰郎哥ᯤ²🐬知行合一', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '“点灯十年，照亮未来！十年磨剑无人知，一朝出鞘惊天下。”@钰郎哥 ', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://boke.qqllyy.cloudns.org/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '钰郎哥ᯤ¹🐬, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '钰郎哥ᯤ²🐬, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
@@ -86,7 +86,7 @@ const BLOG = {
   FACEBOOK_PAGE_ID: process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID || '', // Facebook Page ID 來啟用 messenger 聊天功能
   FACEBOOK_APP_ID: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID || '', // Facebook App ID 來啟用 messenger 聊天功能 获取: https://developers.facebook.com/
 
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '©♥钰郎哥ᯤ¹🐬', // 备案号 闽ICP备XXXXXXX
+  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '©♥钰郎哥ᯤ²🐬', // 备案号 闽ICP备XXXXXXX
 
   // PrismJs 代码相关
   PRISM_JS_AUTO_LOADER:
@@ -333,7 +333,7 @@ const BLOG = {
   HOME_BANNER_IMAGE:
       process.env.NEXT_PUBLIC_HOME_BANNER_IMAGE || './bg_image.jpg', // 首页背景大图, 会被notion中的封面图覆盖，若无封面图则会使用代码中的 /public/bg_image.jpg 文件
   DESCRIPTION:
-      process.env.NEXT_PUBLIC_DESCRIPTION || '钰郎哥ᯤ¹🐬', // 站点描述，被notion中的页面描述覆盖
+      process.env.NEXT_PUBLIC_DESCRIPTION || '钰郎哥ᯤ²🐬', // 站点描述，被notion中的页面描述覆盖
 
   // 网站图片
   IMG_URL_TYPE: process.env.NEXT_PUBLIC_IMG_TYPE || 'Notion', // 此配置已失效，请勿使用；AMAZON方案不再支持，仅支持Notion方案。 ['Notion','AMAZON'] 站点图片前缀 默认 Notion:(https://notion.so/images/xx) ， AMAZON(https://s3.us-west-2.amazonaws.com/xxx)
